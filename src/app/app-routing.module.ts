e52e7b31f4249c9f55401d65ebb819998d95b00c
sarthak143 <sarthak.tiet@gmail.com>
2021-03-04 20:54:45 +0530

    1 file modified, 3 added, 3 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from "./loginTemplateDriven/login.component";
 import { DatabindingsComponent } from "./databindings/databindings.component";
 import { PipeExampleComponent } from "./pipe-example/pipe-example.component";
 import { NotfoundComponent } from "./notfound/notfound.component";
