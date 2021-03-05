e937aee6ef8e4a5e887e0731dc5441d36182a8c5
sarthak143 <sarthak.tiet@gmail.com>
2021-03-05 15:40:23 +0530

    1 file modified

--- src/app/RXJS/sibling1/sibling1.component.ts
+++ src/app/RXJS/sibling1/sibling1.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
 @Component({
-  selector: 'app-sibling1',
-  templateUrl: './sibling1.component.html',
-  styleUrls: ['./sibling1.component.css']
+  selector: "app-sibling1",
+  templateUrl: "./sibling1.component.html",
+  styleUrls: ["./sibling1.component.css"]
 })
 export class Sibling1Component implements OnInit {
-
-  constructor() { }
+  subjectvar = new Subject();
+  constructor() {}
 
   ngOnInit() {
+    for (let i = 0; i < 10; i++) {
+      this.subjectvar.next(i);
+    }
   }
-
 }
