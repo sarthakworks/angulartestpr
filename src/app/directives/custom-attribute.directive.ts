d674280ce6dc59f01b79251ea7581e964cffb7ab
sarthak143 <sarthak.tiet@gmail.com>
2021-03-08 16:18:09 +0530

    2 files modified

--- src/app/directives/custom-attribute.directive.ts
+++ src/app/directives/custom-attribute.directive.ts
@@ -1,14 +1,15 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
-  selector: "[appCustomDirectives]"
+  selector: "[greenBgDirective]"
 })
 export class CustomDirective implements OnInit {
-  constructor(private er: ElementRef) {
+  constructor(private er: ElementRef, private renderer: Renderer2) {
     // this.er.nativeElement.style.backgroundColor = "green"; // better to use in ngOnInit
   }
 
   ngOnInit() {
-    this.er.nativeElement.style.backgroundColor = "green";
+    // this.er.nativeElement.style.backgroundColor = "green"; // can give error in service worker
+    this.renderer.setStyle(this.er.nativeElement, "background-Color", "blue"); // better as Dom is not required 
   }
 }
