a54a5125938dcafbaba292489d620c5f119dec6b
sarthak143 <sarthak.tiet@gmail.com>
2021-03-08 23:49:58 +0530

    1 file modified

--- src/app/directives/custom-attribute.directive.ts
+++ src/app/directives/custom-attribute.directive.ts
@@ -1,9 +1,20 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2
+} from "@angular/core";
 
 @Directive({
   selector: "[CustomAttrDir]"
 })
 export class CustomDirective implements OnInit {
+  @Input() colorA = "blue"; // default color if user doesn't give in property
+  @Input() colorB = "yellow";
+  @HostBinding("style.backgroundColor") bgcolor: string = this.colorA;
   constructor(private er: ElementRef, private renderer: Renderer2) {
     // this.er.nativeElement.style.backgroundColor = "green"; // better to use in ngOnInit
   }
@@ -11,5 +22,15 @@ export class CustomDirective implements OnInit {
   ngOnInit() {
     this.er.nativeElement.style.backgroundColor = "green"; // can give error in service worker
     this.renderer.setStyle(this.er.nativeElement, "border", "3px solid red"); //using renderer is better as Dom is not required
+  }
+
+  @HostListener("mouseenter") xyzMethod(e: Event) {
+    // this.renderer.setStyle(this.er.nativeElement, "background-color", "red");
+    // this.bgcolor = 'red';
+    this.bgcolor = this.colorB; // used for custom color by user
+  }
+  @HostListener("mouseleave") abcMethod(e: Event) {
+    // this.renderer.setStyle(this.er.nativeElement, "background-color", "green");
+    this.bgcolor = this.colorA;
   }
 }
