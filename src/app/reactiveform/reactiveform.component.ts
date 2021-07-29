6a64b4adccdffc5a29766085d776fe206577af87
sarthak143 <sarthak.tiet@gmail.com>
2021-07-29 21:00:28 +0530

    2 files modified, 1 added

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,22 +1,66 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from './_helpers/must-match.validator';
 @Component({
-  selector: "app-reactiveform",
-  templateUrl: "./reactiveform.component.html",
-  styleUrls: ["./reactiveform.component.css"]
+  selector: 'app-reactiveform',
+  templateUrl: './reactiveform.component.html',
+  styleUrls: ['./reactiveform.component.css']
 })
-export class ReactiveformComponent {
-  title = "Angular Form Validation Tutorial";
-  angForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.createForm();
+export class ReactiveformComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        title: ['', Validators.required],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        // validates date format yyyy-mm-dd
+        dob: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+            )
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      }
+    );
   }
-  createForm() {
-    this.angForm = this.fb.group({
-      // name: ["", Validators.required],
-      email: ["", [Validators.required, Validators.email]],
-      password: ["", [Validators.required, Validators.minLength(6)]]
-    });
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert(
+      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4)
+    );
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 }
